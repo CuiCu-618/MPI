7877d61a4bfe052e3f882673cdfddb21aa07e91f
Cui Cu <prc_cc@163.com>
2019-12-07 14:40:28 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/MPI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MPI.dir/main.cpp.o"
+  "MPI"
+  "MPI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "mpi.h"
+
+using namespace std;
+
+int main()
+{
+    int my_rank, comm_sz;
+    int buffer[10];
+    MPI_Status status;
+
+    MPI_Init(nullptr, nullptr);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    if (my_rank == 0) {
+        for (int i = 0; i < 10; i++) {
+            buffer[i] = i;
+        }
+        cout << "proc " << my_rank << " sending..." << endl;
+        MPI_Ssend(buffer, 10, MPI_INT, 1, 123, MPI_COMM_WORLD);
+    }
+
+    if (my_rank == 1){
+        for (int i = 0; i < 10; i++) {
+            buffer[i] = -1;
+        }
+        cout << "proc " << my_rank << " receiving..." << endl;
+        MPI_Recv(buffer, 10, MPI_INT, 0, 123, MPI_COMM_WORLD, &status);
+        for (int i = 0; i < 10; i++){
+            if (buffer[i] != i)
+                cout << "Error: " << buffer[i] << "=" << i << endl;
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
